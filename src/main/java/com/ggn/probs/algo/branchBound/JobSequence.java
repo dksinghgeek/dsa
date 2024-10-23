626d9da217b6522c21f6f2ce43e20d5d343707b7
dsingh-deveoper <165241504+dsingh-deveoper@users.noreply.github.com>
2024-10-23 12:34:47 +0530

    knapsack branch and bound

--- src/main/java/com/ggn/probs/algo/branchBound/JobSequence.java
+++ src/main/java/com/ggn/probs/algo/branchBound/JobSequence.java
@@ -6,37 +6,41 @@ import java.util.List;
 public class JobSequence {
 	private static int UPPER_LIMIT = Integer.MAX_VALUE;
 	private static int NODE_NO = 1;
-	
-	public static int jobSequenceSolution(int[][] cost) {	
-		List<List<Job>> solution = new ArrayList<>();
+
+	public static void jobSequenceSolution(int[][] cost) {
 		List<Job> queue = new ArrayList<>();
-		for(int i = 0 ; i < cost[0].length; i++) {
-			
+		for (int i = 0; i < cost[0].length; i++) {
+
 			Job job = new Job(NODE_NO++);
 			job.jobId = i;
 			int calculatedCost = calNSetCost(cost, job, i);
-			System.out.println(calculatedCost);
-			if(calculatedCost > UPPER_LIMIT)
+			if (calculatedCost > UPPER_LIMIT)
 				continue;
 			job.cost = calculatedCost;
-			
+
 			job.upper = calNSetUpper(cost, job);
-			System.out.println(job.upper);
-			
-			if(job.upper < UPPER_LIMIT) {
+
+			if (job.upper < UPPER_LIMIT) {
 				UPPER_LIMIT = job.upper;
 			}
-			
+
+			job.deadline = cost[2][i];
+			job.time = cost[3][i];
+
 			queue.add(job);
 		}
-		
+
 		while (!queue.isEmpty()) {
 			Job persistetedJob = queue.remove(0);
-			
-			for (int i = persistetedJob.jobId+1; i < cost[0].length; i++) {
+
+			if (persistetedJob.upper > UPPER_LIMIT)
+				continue;
+
+			for (int i = persistetedJob.jobId + 1; i < cost[0].length; i++) {
 				int jobId = findNextJob(persistetedJob, cost, i);
 				Job job = new Job(NODE_NO++);
 				job.jobId = jobId;
+				persistetedJob.child = job;
 				job.parent = persistetedJob;
 				int calculatedCost = calNSetCost(cost, job, jobId);
 				if (calculatedCost > UPPER_LIMIT)
@@ -48,18 +52,43 @@ public class JobSequence {
 					UPPER_LIMIT = job.upper;
 				}
 
+				job.deadline = cost[2][i];
+				job.time = cost[3][i];
+
 				queue.add(job);
 			}
-		}
-		
 
-		return UPPER_LIMIT;
+			if (queue.isEmpty()) {
+				Job temp = persistetedJob;
+				Job head = null;
+
+				int maxDeadline = Integer.MIN_VALUE;
+				while (temp != null) {
+					if (temp.deadline > maxDeadline)
+						maxDeadline = temp.deadline;
+
+					if (temp.parent == null)
+						head = temp;
+
+					temp = temp.parent;
+				}
+
+				List<Job> output = new ArrayList<>();
+				while (maxDeadline > 0 && head != null) {
+					output.add(head);
+					maxDeadline = maxDeadline - head.time;
+					head = head.child;
+				}
+
+				output.stream().forEach(t -> System.out.print(t.jobId + " "));
+			}
+		}
 	}
 
 	private static int findNextJob(Job job, int[][] cost, int id) {
 		int jobId = -1;
-		for(int i = id ; i < cost[0].length; i++) {
-			if(!isJobExits(job, i)) {
+		for (int i = id; i < cost[0].length; i++) {
+			if (!isJobExits(job, i)) {
 				jobId = i;
 				break;
 			}
@@ -81,8 +110,8 @@ public class JobSequence {
 
 	private static boolean isJobExits(Job job, int j) {
 		Job temp = job;
-		while(temp != null) {
-			if(temp.jobId == j)
+		while (temp != null) {
+			if (temp.jobId == j)
 				return true;
 			temp = temp.parent;
 		}
@@ -102,14 +131,10 @@ public class JobSequence {
 	}
 
 	public static void main(String[] args) {
-		int cost[][] = { 
-				{ 1, 2,  3, 4},
-				{ 5, 10, 6, 3 }, 
-				{ 1, 3,  2, 1 }, 
-				{ 1, 2,  1, 1 } };
-				
+		int cost[][] = { { 1, 2, 3, 4 }, { 5, 10, 6, 3 }, { 1, 3, 2, 1 }, { 1, 2, 1, 1 } };
+
 		jobSequenceSolution(cost);
-		
+
 	}
 
 }
@@ -119,8 +144,11 @@ class Job {
 	int upper;
 	int cost;
 	int jobId;
+	int deadline;
+	int time;
 	Job parent;
-	
+	Job child;
+
 	public Job(int nodeNo) {
 		this.nodeNo = nodeNo;
 	}
