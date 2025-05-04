75fcab996c4278530c75b0bfb18ccf0da4ae19a0
Dharmendra Singh <dksingh.work@outlook.com>
2025-05-04 10:17:01 +0530

    MaxSumAllSubArraysOfSizeK

---
+++ src/main/java/com/ggn/probs/algo/slidingwindow/MaxSumAllSubArraysOfSizeK.java
@@ -0,0 +1,42 @@
+package com.ggn.probs.algo.slidingwindow;
+
+public class MaxSumAllSubArraysOfSizeK {
+
+	public void maxSumAllSubArraysOfSizeK(int[] A, int K) {
+		int n = A.length;
+		int sum = 0;
+		int MAX = Integer.MIN_VALUE;
+
+		int i = 0;
+		int j = 0;
+
+		while (j < n) {
+			if (j - i + 1 < K) {
+				sum += A[j];
+				j++;
+			} else if (j - i + 1 == K) {
+				sum += A[j];
+
+				if (sum > MAX) {
+					MAX = sum;
+					System.out.print(" : " + MAX);
+				}
+
+				sum -= A[i];
+				i++;
+				j++;
+			}
+		}
+
+		System.out.println("\nMax : " + MAX);
+	}
+
+	public static void main(String[] args) {
+		int[] A = { 1, 3, -1, -3, 5, 3, 6, 7 };
+		int K = 3;
+
+		MaxSumAllSubArraysOfSizeK op = new MaxSumAllSubArraysOfSizeK();
+		op.maxSumAllSubArraysOfSizeK(A, K);
+	}
+
+}
